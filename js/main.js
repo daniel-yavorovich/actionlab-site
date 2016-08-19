868ca83508a81e94fdbd318063fa0206cc540bd2
Daniel Yavorovych <daniel@yavorovych.com>
2016-08-19 21:27:34 +0300

    * add photo gallery * disable video autoplay

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 $(document).ready(function () {
     $('#fullpage').fullpage({
         menu: '#menu',
-        anchors: ['section0', 'section1', 'section2', 'section3', 'section4'],
+        anchors: ['section0', 'section1', 'section2', 'section3', 'section4', 'section5'],
         scrollingSpeed: 1000,
         loopBottom: false,
         paddingTop: '53px',
         paddingBottom: '53px',
-        responsiveHeight: 900,
         verticalCentered: true,
         scrollOverflow: true,
         scrollOverflowOptions: {
@@ -17,4 +16,29 @@ $(document).ready(function () {
             disableMouse: true
         }
     });
+
 });
+
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: 'NoSIZIIFOxs',
+        events: {
+            'onReady': onPlayerReady,
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    player.stopVideo();
+}
