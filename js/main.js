aae46377c67bd7316b6fe5e7767d00e4e670b586
Daniel Yavorovych <daniel@yavorovych.com>
2016-08-19 18:56:17 +0300

    add social buttons

--- js/main.js
+++ js/main.js
@@ -2,12 +2,13 @@ $(document).ready(function () {
     $('#fullpage').fullpage({
         menu: '#menu',
         anchors: ['section0', 'section1', 'section2', 'section3', 'section4'],
-        scrollOverflow: true,
-        scrollingSpeed: 1500,
+        scrollingSpeed: 1000,
         loopBottom: false,
         paddingTop: '53px',
         paddingBottom: '53px',
+        responsiveHeight: 900,
         verticalCentered: true,
+        scrollOverflow: true,
         scrollOverflowOptions: {
             scrollbars: true,
             mouseWheel: true,
