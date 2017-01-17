4693b9553d2a5637629f49d922f965ef8248526d
Aleksey Bulash <alexeybulash@gmail.com>
2017-01-17 22:46:37 +0200

    Commented full page plugin. Added some text changes. Fixed containers

--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
 $(document).ready(function () {
-    $('#fullpage').fullpage({
-        menu: '#menu',
-        anchors: ['section0', 'section1', 'section2', 'section3', 'section4', 'section5'],
-        scrollingSpeed: 1000,
-        loopBottom: false,
-        paddingTop: '53px',
-        paddingBottom: '53px',
-        verticalCentered: true,
-        scrollOverflow: true,
-        scrollOverflowOptions: {
-            scrollbars: true,
-            mouseWheel: true,
-            hideScrollbars: false,
-            fadeScrollbars: false,
-            disableMouse: true
-        }
-    });
+    if ($('#fullpage').fullpage) {
+        $('#fullpage').fullpage({
+            menu: '#menu',
+            anchors: ['section0', 'section1', 'section2', 'section3', 'section4', 'section5'],
+            scrollingSpeed: 1000,
+            loopBottom: false,
+            paddingTop: '53px',
+            paddingBottom: '53px',
+            verticalCentered: true,
+            scrollOverflow: true,
+            scrollOverflowOptions: {
+                scrollbars: true,
+                mouseWheel: true,
+                hideScrollbars: false,
+                fadeScrollbars: false,
+                disableMouse: true
+            }
+        });
+    }
 
 });
 
